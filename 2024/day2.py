e53e7343b8a65b7c5cf0aa3fefe51223dc2b2a6d
mrfolksy <mrfolksy@gmail.com>
2024-12-02 10:25:09 +0000

    aoc 2024 - day2 part1

--- 2024/day2.py
+++ 2024/day2.py
@@ -1,22 +1,43 @@
+from functools import reduce
+
+
 with open("./data/day2.txt") as f:
     data = f.readlines()
 
-def parse_line(line): 
+
+def parse_line(line):
     return [int(i) for i in line.split(" ")]
 
-def calc_deltas(report_vals):
-    return [
-        report_vals[1] - report_vals[0],
-        report_vals[2] - report_vals[1],
-        report_vals[3] - report_vals[2],
-        report_vals[4] - report_vals[3],
-        report_vals[5] - report_vals[4],
-    ]
 
-def is_safe_report(report_vals):
-    deltas = calc_deltas(report_vals)
-    
+def calc_deltas(report_vals):
+    deltas = []
+
+    for i in range(len(report_vals) - 1, 0, -1):
+        delta = report_vals[i] - report_vals[i - 1]
+        deltas = [delta, *deltas]
+
+    return deltas
+
+
+def meets_conditions(lst, condition):
+    return all(condition(item) for item in lst)
+
+
+def is_safe_report(deltas):
+    return (
+        meets_conditions(deltas, lambda x: x > 0)
+        or meets_conditions(deltas, lambda x: x < 0)
+    ) and meets_conditions(deltas, lambda x: 1 <= abs(x) <= 3)
+
 
 def part1():
-    for line in data:
-        report_vals = parse_line(line)
+    total_safe = 0
+    for report in data:
+        report_vals = parse_line(report)
+        deltas = calc_deltas(report_vals)
+        total_safe += 1 if is_safe_report(deltas) else 0
+
+    print(total_safe)
+
+
+part1()
