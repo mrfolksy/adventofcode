353f67872569d74790fe11f75cb1eae8be0d82bb
Your Name <you@example.com>
2024-12-06 12:28:34 +0000

    WIP - day 5 - part 2

--- 2024/day5.py
+++ 2024/day5.py
@@ -1,4 +1,4 @@
-with open("./data/day5.txt") as f:
+with open("./data/day5.test.txt") as f:
     input = f.read().split("\n")
 
 rules = set()
@@ -12,18 +12,25 @@ for line in input:
 
 
 def is_valid_update(rules: set[str], update: list[int]):
+    violations = {}
     for i, _ in enumerate(update):
         for _, v in enumerate(update[i + 1 :]):
             if f"{update[i]}|{v}" not in rules:
-                return False
-    return True
+                violations[i] = violations.get(i, 0) + 1
+
+    return (len(violations) == 0, violations)
+
+
+def fix_invalid_update(update: list[int], violations: list[str]):
+    pass
 
 
 def part1(rules, updates):
     valid_updates = []
     for update in updates:
         update = [int(s) for s in update.split(",")]
-        if is_valid_update(rules, update):
+        is_valid, _ = is_valid_update(rules, update)
+        if is_valid:
             valid_updates.append(update)
 
     total = 0
@@ -33,4 +40,22 @@ def part1(rules, updates):
     return total
 
 
-print(part1(rules, updates))
+def part2(rules, updates):
+    fixed_updates = []
+
+    for update in updates:
+        update = [int(s) for s in update.split(",")]
+        is_valid, violations = is_valid_update(rules, update)
+        if not is_valid:
+            print(update)
+            print(violations)
+
+    # total = 0
+    # for update in fixed_updates:
+    #     total += update[int(len(update) / 2)]
+
+    # return total
+
+
+# print(part1(rules, updates))
+print(part2(rules, updates))
