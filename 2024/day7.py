0c7ce16a0d0ff4933dc46ce6ba01ca319adf536e
mrfolksy <mrfolksy@gmail.com>
2024-12-09 13:46:28 +0000

    day 7 - part 1

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,2 +1,48 @@
-with open("./data/day7.test.txt") as f:
+from itertools import combinations, product
+
+with open("./data/day7.txt") as f:
     input = f.read().split("\n")
+
+# key = number of slots
+# value = an arrary of all possible combinations of + and *
+combinations_map = {}
+
+
+def get_combinations(n: int):
+    if n not in combinations_map:
+        combinations_map[n] = [c for c in product(["+", "*"], repeat=n)]
+    return combinations_map[n]
+
+
+def parse_equation(line: str):
+    parts = line.split(":")
+    return (int(parts[0]), [int(n) for n in parts[1][1:].split(" ")])
+
+
+def evaluate(numbers: list[int], operators: list[str]):
+    total = 0
+
+    for i, v in enumerate(operators):
+        lhs = total if total != 0 else numbers[i]
+        rhs = numbers[i + 1]
+        total = eval(f"{lhs}{v}{rhs}")
+
+    return total
+
+
+def part1(input):
+    ans = 0
+
+    for line in input:
+        test_val, numbers = parse_equation(line)
+
+        for c in get_combinations(len(numbers) - 1):
+            result = evaluate(numbers, c)
+            if result == test_val:
+                ans += result
+                break
+
+    return ans
+
+
+print(part1(input))
