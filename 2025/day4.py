1e3ce8ead940d7cbff8d4ee61a09c0c4eed24188
chris gell <chris@MacBookPro.localdomain>
2025-12-05 17:40:24 +0000

    2025 day4 - part 2

--- 2025/day4.py
+++ 2025/day4.py
@@ -1,34 +1,53 @@
-data = []
+input = []
 with open("data/day4.txt") as f:
     while l := f.readline():
-        data.append(l.strip())
+        input.append(list(l.strip()))
 
 
-def part1(data):
-    def count_rolls(i, j):
-        """
-          -1
-        -1 0 1
-           1
-        """
-        count = 0
+def can_remove_roll(data, i, j):
+    count = 0
 
-        xr = range(max(0, i - 1), min(len(data), i + 2))
-        yr = range(max(0, j - 1), min(len(data), j + 2))
-        for x in xr:
-            for y in yr:
-                if x == i and y == j:
-                    continue
-                count += 1 if data[x][y] == "@" else 0
+    xr = range(max(0, i - 1), min(len(data), i + 2))
+    yr = range(max(0, j - 1), min(len(data), j + 2))
+    for x in xr:
+        for y in yr:
+            if x == i and y == j:
+                continue
+            count += 1 if data[x][y] == "@" else 0
 
-        return count
+    return count < 4
 
+
+def part1(input):
     res = 0
-    for i in range(len(data[0])):
-        for j in range(len(data[i])):
-            if data[i][j] == "@" and count_rolls(i, j) < 4:
+    for i in range(len(input[0])):
+        for j in range(len(input[i])):
+            if input[i][j] == "@" and can_remove_roll(input, i, j):
                 res += 1
     return res
 
 
-print(f"Part 1 - {part1(data)}")
+def part2(input):
+    res = 0
+
+    data = input
+    while True:
+        rolls_taken = []
+        for i in range(len(data[0])):
+            for j in range(len(data[i])):
+                if data[i][j] == "@" and can_remove_roll(data, i, j):
+                    rolls_taken.append((i, j))
+
+        if len(rolls_taken) == 0:
+            break
+
+        else:
+            res += len(rolls_taken)
+            for r in rolls_taken:
+                data[r[0]][r[1]] = "x"
+
+    return res
+
+
+print(f"Part 1 - {part1(input)}")
+print(f"Part 2 - {part2(input)}")
